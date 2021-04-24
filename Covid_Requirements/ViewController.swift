a9d4e8c60a038ca59a0134f40c02dbb4c11dd941
Paritosh Arora <parigames@gmail.com>
2021-04-24 13:23:19 +0530

    Initialised

--- Covid_Requirements/ViewController.swift
+++ Covid_Requirements/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var optionBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func optionBtnClicked(_ sender: UIButton) {
+    }
+    
 }
 
